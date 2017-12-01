a5874ba67747368da7653d9b72203e4fee613379
g-hyoga <hyoga0216@gmail.com>
2017-12-01 13:24:25 +0900

    add html

---
+++ question.js
@@ -0,0 +1,16 @@
+const questionsExample = {
+  questions: [
+    {
+      statement: "問題文",
+      choices: ["回答1", "回答2", "回答3", "回答4"],
+      answer: 1,
+      img: "img/画像の名前.png",
+    },
+    {
+      statement: "問題文",
+      choices: ["回答1", "回答2", "回答3", "回答4"],
+      answer: 1,
+      img: "img/画像の名前.png",
+    }
+  ]
+}
