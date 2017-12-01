fe4b7a43afed0bc9235471cc3b24009b1853f982
g-hyoga <hyoga0216@gmail.com>
2017-12-01 13:30:45 +0900

    modify question

--- question.js
+++ question.js
@@ -5,12 +5,14 @@ const questionsExample = {
       choices: ["回答1", "回答2", "回答3", "回答4"],
       answer: 1,
       img: "img/画像の名前.png",
+      commentary: "解説(あれば)",
     },
     {
       statement: "問題文",
       choices: ["回答1", "回答2", "回答3", "回答4"],
       answer: 1,
       img: "img/画像の名前.png",
+      commentary: "解説(あれば)",
     }
   ]
 }
