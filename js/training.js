41baadae0a36b1b01b88e38632944229f3a2fae5
g-hyoga <hyoga0216@gmail.com>
2017-12-05 03:54:51 +0900

    training

---
+++ js/training.js
@@ -0,0 +1,19 @@
+const init = () => {
+  const training = JSON.parse(localStorage.getItem("training"));
+
+  training.forEach((e) => {
+    const imgDiv = document.createElement('div');
+    imgDiv.setAttribute("class", "img-div");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", e.img);
+    imgDiv.appendChild(img);
+
+    const imgStatement = document.createElement("p");
+    imgStatement.setAttribute("class", "statement");
+    imgStatement.textContent = e.explain;
+    imgDiv.appendChild(imgStatement);
+
+    document.getElementById("wrapper").appendChild(imgDiv);
+  });
+}
