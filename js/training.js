ed12a5308022f88011aef5216b572ae520c33e82
g-hyoga <hyoga0216@gmail.com>
2017-12-05 04:00:10 +0900

    add title

--- js/training.js
+++ js/training.js
@@ -14,6 +14,6 @@ const init = () => {
     imgStatement.textContent = e.explain;
     imgDiv.appendChild(imgStatement);
 
-    document.getElementById("wrapper").appendChild(imgDiv);
+    document.getElementById("content").appendChild(imgDiv);
   });
 }
